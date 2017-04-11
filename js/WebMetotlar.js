1dd586fc54b6f9668f16bf330e4e686aa29024bf
Ahmet <ahmet.bal2000@gmail.com>
2017-04-11 14:50:10 +0300

    dosya eklemeleri

---
+++ js/WebMetotlar.js
@@ -0,0 +1,26 @@
+﻿var webMetotlarUrl = "/WebServisler/AutoComplete.asmx";
+$(function () {
+    $(".kredikartino").blur(function (e) {
+        var krediKartiNo = $(this).val();
+        BankaAdiGetir(krediKartiNo);
+        alert(bankaAdi);
+    });
+});
+
+
+function BankaAdiGetir(krediKartiNo) {
+    var bankaAdi = krediKartiNo;
+    $.ajax({
+        type: "POST",
+        url: webMetotlarUrl + "/BankaAdiGetir",
+        data: "{'krediKartiNo':'" + krediKartiNo + "'}",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (msg) {
+            return msg.d;
+        },
+        error: function (err) {
+            return '';
+        }
+    });
+}
---
+++ js/login.js
@@ -0,0 +1,16 @@
+﻿function SearchString(source, arguments) {
+    var arrChars = ['-', '/', '\\', '*', '\'', '\"', '{', '}', '[', ']', '(', ')', '#', '+', '&'];
+    var sText1 = document.getElementById('txtKullaniciAdi').value;
+    var sText2 = document.getElementById('txtSifre').value;
+    var flag = false;
+    for (var i = 0; i < arrChars.length; i++) {
+        if (sText1.indexOf(arrChars[i]) != -1)
+        { flag = true; }
+        if (sText2.indexOf(arrChars[i]) != -1)
+        { flag = true; }
+    }
+    if (flag) {
+        arguments.IsValid = false;
+        alert('Geçersiz karakter - / \\ * \" \' { } [ ] ( ) # + & kullandınız!!');
+    }
+}
